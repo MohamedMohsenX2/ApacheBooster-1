c5844d4069e71d1aad063a40a1f1c998e58f052e
Shubham Mathur <itseasy21@gmail.com>
2014-09-20 11:09:43 +0530

    updates upgrade instructions

--- apachebooster/cgi/ApacheBooster/upgrade.php
+++ apachebooster/cgi/ApacheBooster/upgrade.php
@@ -1,6 +1,6 @@
 <?php
-$RVERSION = `/usr/bin/curl -s http://prajith.in/version.txt`;
-$LVERSION = `/bin/cat /usr/local/cpanel/whostmgr/cgi/ApacheBooster/version.txt`;
+$RVERSION = '/usr/bin/curl -s https://raw.githubusercontent.com/itseasy21/ApacheBooster/master/apachebooster/cgi/ApacheBooster/version.txt';
+$LVERSION = '/bin/cat /usr/local/cpanel/whostmgr/cgi/ApacheBooster/version.txt';
 ?>
 <!DOCTYPE html>
 <html>
@@ -11,14 +11,15 @@ $LVERSION = `/bin/cat /usr/local/cpanel/whostmgr/cgi/ApacheBooster/version.txt`;
 <body>
 <div id="contentMain" class="contentPadding">
 <?php
-                          if ($LVERSION==$RVERSION)
+                          if ($LVERSION == $RVERSION){
                echo "<div class=\"success\">
-<p>   Already uptodate </p></div>";
+<p>   You are using the latest version! </p></div>";
                           
-                          else
+                          }else{
 echo "<div class=\"error\">
-                                Version $RVERSION is now available. Please reffer the  belw docs to upgrade.
+                                Version $RVERSION is now available. Please refer the below docs to upgrade.
                           </div>";
+						  }
                           ?>
 
 
@@ -30,8 +31,7 @@ echo "<div class=\"error\">
             <div id="welcomemessage">
         <p><pre>Please upgrade the ApacheBooster package only from Terminal. 
 You can simply upgrade it by running  predefined script: /scripts/updateapachebooster
-Gmail: prajithpalakkuda@gmail.com
-skype: prajithp80
+Or visit following link: https://github.com/itseasy21/ApacheBooster for more info
         </div>
         </div>
 
