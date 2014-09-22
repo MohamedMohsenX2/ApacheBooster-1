5240f806f0c72fcd7e3cbc0dc9e9f4402a5e6119
Shubham Mathur <itseasy21@gmail.com>
2014-09-22 19:10:11 +0530

    Reuploaded files

--- apachebooster/cgi/ApacheBooster/footer.php
+++
@@ -1,9 +0,0 @@
-<div class="footer">
-<span id="valid">
-Copyright &copy; 2011-2012<a href="https://github.com/itseasy21/ApacheBooster">cPanel ApacheBooster</a> <br />
-Powered by Github</a>
-</span>
-</div>
-</body>
-</html>
-
