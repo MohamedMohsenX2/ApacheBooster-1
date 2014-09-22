4295373d9d4f59caef2cf9735aae488a0a3472d2
Shubham Mathur <itseasy21@gmail.com>
2014-09-22 19:25:08 +0530

    Fixed Typos

--- apachebooster/cgi/ApacheBooster/footer.php
+++ apachebooster/cgi/ApacheBooster/footer.php
@@ -1,7 +1,7 @@
 <div class="footer">
 <span id="valid">
 Copyright &copy; 2011-2012<a href="https://github.com/itseasy21/ApacheBooster/ "> cPanel ApacheBooster</a> <br />
-Powered  by  Prajith</a>
+Powered  by <a href="https://github.com/itseasy21/">Github</a>
 </span>
 </div>
 </body>
